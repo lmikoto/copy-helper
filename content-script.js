4e2082bf1311a41de1cce25ff93914bd203edb94
liuyang <ly193933@alibaba-inc.com>
2020-07-30 13:19:14 +0800

    init

---
+++ content-script.js
@@ -0,0 +1,23 @@
+﻿const config = [
+	{
+		url: 'segmentfault.com',
+		pattern: /(\s*)\n作者[\s\S]*著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。[\s\S]*$/
+	},
+	{
+		url: 'jianshu.com',
+		pattern: /(\s*)\n作者[\s\S]*著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。[\s\S]*$/
+	}
+]
+
+document.addEventListener("copy", async ()=>{
+	for(c of config){
+		if(window.location.href.indexOf(c.url) != -1){
+			const text = await navigator.clipboard.readText();
+			if(c.pattern.test(text)){
+				const replace = text.replace(c.pattern,'');
+				console.log(`to replace clipboard from ${text} to ${replace}`)
+				await navigator.clipboard.writeText(replace);
+			}
+		}
+	}
+} );
